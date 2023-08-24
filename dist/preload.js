e7b15de52e35f9f9297997cdc0cdeec66f1a004b
Waua <waua@outlook.com>
2023-08-24 23:43:19 +0800

    Fix #14 fixup settings

--- dist/preload.js
+++ dist/preload.js
@@ -5,7 +5,7 @@ window.writeFile = (path, data, opts) => {
   const { encoding, isArrayBuffer } = Object.assign(
     // default options
     { encoding: "utf8", isArrayBuffer: false },
-    opts
+    opts,
   );
   if (isArrayBuffer) data = Buffer.from(data);
   return new Promise((resolve, reject) => {
@@ -28,17 +28,31 @@ window.readFileSync = fs.readFileSync;
   if (scenes) {
     if (!Array.isArray(scenes.value)) return;
     utools.db.bulkDocs(
-      scenes.value.map((scene) => ({ _id: `scene/${scene.id}`, value: scene }))
+      scenes.value.map((scene) => ({ _id: `scene/${scene.id}`, value: scene })),
     );
     utools.db.remove(KEY_SCENES);
   }
+})();
+
+(function fixup() {
+  const SETTINGS_KEY = "settings";
+  const DEFAULT_WIDTH = 300;
+  const MIN_WIDTH = 90;
+  const settings = utools.dbStorage.getItem(SETTINGS_KEY);
+  if (!settings) {
+    return;
+  }
+  utools.dbStorage.setItem(SETTINGS_KEY, {
+    ...settings,
+    asideWidth: asideWidth < MIN_WIDTH ? DEFAULT_WIDTH : asideWidth,
+  });
 })();
 
 utools.onMainPush(({ code, payload, type }) => {
   const scenes = utools.db.allDocs("scene/");
   return scenes
     .filter(
-      ({ value: { name, deleted } }) => !deleted && name.includes(payload)
+      ({ value: { name, deleted } }) => !deleted && name.includes(payload),
     )
     .map((scene) => ({
       text: scene.value.name ?? scene._id,
