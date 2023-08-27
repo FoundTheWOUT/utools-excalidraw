a44af3fe0ad3fd25450436f152dc2747335ef0a9
Waua <waua@outlook.com>
2023-08-27 16:39:53 +0800

    Optimize switch animation

--- src/ui/Switch.tsx
+++ src/ui/Switch.tsx
@@ -1,26 +1,22 @@
 import cn from "clsx";
+import "./Switch.css";
 
 function Switch({
   checked,
   notAllow = false,
   ...rest
-}: { checked: boolean; notAllow?: boolean } & JSX.IntrinsicElements["button"]) {
+}: { notAllow?: boolean } & JSX.IntrinsicElements["input"]) {
   return (
-    <button
+    <input
+      type="checkbox"
+      disabled={notAllow}
+      checked={checked}
       className={cn(
-        "relative flex h-6 w-10 items-center rounded-full",
-        checked ? "bg-[#6965db]" : "bg-gray-300",
+        "switch relative",
         notAllow ? "cursor-not-allowed" : "cursor-pointer",
       )}
       {...rest}
-    >
-      <span
-        className={cn(
-          "inline-block h-5 w-5 rounded-full bg-white transition-transform",
-          checked ? "translate-x-[1.1rem]" : "translate-x-[0.1rem]",
-        )}
-      />
-    </button>
+    />
   );
 }
 
